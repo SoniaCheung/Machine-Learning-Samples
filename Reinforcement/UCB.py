6991207970724ecc820ed3e1fcf2843d1e8e3f41
SoniaCheung <soniacym@gmail.com>
2018-10-02 20:07:28 +0800

    Reinforcement python file with dataset

---
+++ Reinforcement/UCB.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  2 18:10:16 2018
+
+@author: sonia
+
+Upper Confidence Bound
+"""
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Imporing the dataset
